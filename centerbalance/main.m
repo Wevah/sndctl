e00043181d167180f04e7a34530a39cf264e9271
Nate Weaver <wevah@derailer.org>
2017-01-13 03:47:59 -0600

    Initial Commit

---
+++ centerbalance/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  centerbalance
+//
+//  Created by Nate Weaver on 2017-01-13.
+//  Copyright © 2017 Nate Weaver/Derailer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
